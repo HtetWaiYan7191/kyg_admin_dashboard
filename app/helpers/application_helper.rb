81962c74163979d156ca4293f44e5ae9692f4843
htetwaiyan <htetwaiyan7191@gmail.com>
2024-05-01 12:07:21 +0630

    add appliation helper methods for sidebar nav active

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+        def dashboard_active
+            request.path == "/" ? " active fw-bolder" : ""
+        end
+    
+        def blog_active
+            request.path == blogs_path ? "active fw-bolder" : ""
+        end
+    
+        def account_active
+            
+        end
+    
+        def testimonial_active
+            request.path == testimonials_path ? "active fw-bolder" : ""
+        end
+    
+        def contact_active
+            request.path == contacts_path ? "active fw-bolder" : ""
+        end
+    
+        def category_active
+            request.path == categories_path ? "active fw-bolder " : ""
+        end
 end
