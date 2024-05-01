5a782e9597626ca5a0b974a88b640a4cea682956
htetwaiyan <htetwaiyan7191@gmail.com>
2024-05-01 12:07:33 +0630

    add controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
+  resources :users
+  get 'contacts/index'
   resources :blogs
   resources :categories
   resources :testimonials
+  resources :contacts 
   namespace :api do
     namespace :v1 do
       resources :contacts
     end
   end
   root to: 'home#index'
-  get '/home/index', to:'home#index'
+  get '/home/index', to:'home#index', as: :home_page
   devise_for :users
 
 end
