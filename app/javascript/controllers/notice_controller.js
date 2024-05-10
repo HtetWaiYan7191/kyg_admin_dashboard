9c1d3c6d2d057564f0e8eb0fcc641e6633ed4151
htetwaiyan <htetwaiyan7191@gmail.com>
2024-05-10 12:24:43 +0630

    Make notice and alert dismissable

---
+++ app/javascript/controllers/notice_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from 'stimulus'
+
+export default class extends Controller { 
+    static targets = ["noticeBox"]
+    connect() {
+        console.log('this is notice js controller')
+    }
+
+    closeNotice() {
+        this.noticeBoxTarget.classList.add('hidden')
+    }
+}
