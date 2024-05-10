8b17d453d3ec49f55b10e1a9494486e030c166ac
htetwaiyan <htetwaiyan7191@gmail.com>
2024-05-10 17:16:51 +0630

    Design update

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  paginates_per 4 
   validates :name, presence: true
   enum role: {user: 0, admin: 1}
 end
