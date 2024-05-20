3000820a7350af3103595bd2d47f1b014bb02f10
htetwaiyan <htetwaiyan7191@gmail.com>
2024-05-20 15:52:37 +0630

    add new migration for kyg users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_08_094206) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_20_070021) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -74,6 +74,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_08_094206) do
     t.string "current_school"
     t.string "current_year_group"
     t.string "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "kings_yangon_users", force: :cascade do |t|
+    t.string "school_id"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
