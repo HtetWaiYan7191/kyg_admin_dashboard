a1b4c47c6ad1f044a30db8c3600221e6c57a244a
htetwaiyan <htetwaiyan7191@gmail.com>
2024-08-18 14:04:38 +0630

    add required information for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,16 +16,6 @@ puts 'management created'
 kyg_user = KingsYangonUser.create(school_id: 'kyg123', password: 'password')
 puts 'kyg user created'
 
-# Contacts 
-conact1 = Contact.create(student_name: 'kyw kyw', parent_name: 'U Ba', email: 'kywkyw@gmail.com', phone_number: '0989652451', birth_date: '2002-01-17', gender: 'male', nationality: 'myanmar', address: 'yangon', current_school: 'Bo 6', current_year_group: 'IGCSE', message: 'interested')
-conact2 = Contact.create(student_name: 'su su ', parent_name: 'U Aung', email: 'susu@gmail.com', phone_number: '0989652423', birth_date: '2002-05-17', gender: 'female', nationality: 'myanmar', address: 'yangon', current_school: 'Bo 5', current_year_group: 'IGCSE', message: 'interested')
-
-puts "#{Contact.count} contacts created"
-
-# Items created 
-Item.create(name: 'chicken', content:' best seller', price: 15455, discount_percentage: 10, brand_partner_id: 1 )
-puts "#{Item.count} items created"
-
 accessories = BrandCategory.create(name: 'Accessories', description: 'type of business accessories')
 clothing = BrandCategory.create(name: 'Clothing & Accessory', description: 'type of business clothing and accessory')
 health_and_beauty = BrandCategory.create(name: 'Health & Beauty', description: 'type of business health and beauty')
