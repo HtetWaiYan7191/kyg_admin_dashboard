54f9864389444141688d26702e87876887fb2c10
htetwaiyan <htetwaiyan7191@gmail.com>
2024-08-18 16:07:17 +0630

    fix user table not found error by updaing migrate file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -10,5 +10,5 @@ bundle exec rails assets:clean
 # perform database migrations in the build command.
 # Uncomment the following line:
 
- rails db:migrate
- rails db:seed 
+./bin/rails db:migrate
+./bin/rails server
